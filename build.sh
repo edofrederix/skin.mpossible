d30cbfb4116ed77e121b52ab6744108b97f80de4
mPossible <info@mpossible.nl>
2012-07-25 13:45:12 +0200

    Initial commit
    Includes all original Confluence files, plus some build files

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+BUILDDIR=./build
+CONTENT=addon.xml\ 720p\ backgrounds\ colors\ fonts\ icon.png\ language\ sounds
+     
+
+# Get input
+echo "Enter package name:"
+read PACKAGENAME
+echo "Enter package version:"
+read VERSION
+
+echo "Creating package called $PACKAGENAME-$VERSION.zip. Is this correct (Y/n)"
+read yesno
+
+case $yesno in
+    n|N)
+        echo "Stopping."
+        exit ;;
+    *) ;;
+esac
+
+# Cleanup buil dir
+rm -fr $BUILDDIR/*
+mkdir -p $BUILDDIR/$PACKAGENAME
+
+# Create textures
+#make clean -C ./media
+make -C ./media
+mkdir -p $BUILDDIR/$PACKAGENAME/media
+cp ./media/Textures.xbt $BUILDDIR/$PACKAGENAME/media/
+
+# Copy files
+cp -r $CONTENT $BUILDDIR/$PACKAGENAME/
+
+# Insert version number and addon name
+sed -i 's/<!-- versionnum -->/'$VERSION'/g' $BUILDDIR/$PACKAGENAME/addon.xml
+sed -i 's/<!-- addonname -->/'$PACKAGENAME'/g' $BUILDDIR/$PACKAGENAME/addon.xml
+
+# Create package
+cd $BUILDDIR
+zip -q -r ../$PACKAGENAME-$VERSION.zip $PACKAGENAME
+cd ..
+
+echo "Done."
